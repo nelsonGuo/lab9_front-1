dd5c71151df0b555d93efac7f80a0840bc01864f
nelson Guo <nelsonkwok15@gmail.com>
2017-12-02 16:35:48 -0800

    client side
    Signed-off-by: nelson Guo <nelsonkwok15@gmail.com>

--- application/core/XML_Model.php
+++ application/core/XML_Model.php
@@ -1,70 +1,69 @@
 <?php
-
-
-class XML_Model extends Memory_Model {
+/**
+ * CSV-persisted collection.
+ * 
+ * @author		JLP
+ * @copyright           Copyright (c) 2010-2017, James L. Parry
+ * ------------------------------------------------------------------------
+ */
+class XML_Model extends Memory_Model
+{
+//---------------------------------------------------------------------------
+//  Housekeeping methods
+//---------------------------------------------------------------------------
 
 	protected $xml = null;
-
-
+	/**
+	 * Constructor.
+	 * @param string $origin Filename of the CSV file
+	 * @param string $keyfield  Name of the primary key field
+	 * @param string $entity	Entity name meaningful to the persistence
+	 */
 	function __construct($origin = null, $keyfield = 'id', $entity = null)
-	{	
-
+	{
 		parent::__construct();
-		$origin  = realpath($origin);
 		// guess at persistent name if not specified
-			if ($origin == null)
-				$this->_origin = get_class($this);
-			else
-				$this->_origin = $origin;
-
-			// remember the other constructor fields
-			$this->_keyfield = $keyfield;
-			$this->_entity = $entity;
-
-			// start with an empty collection
-			$this->_data = array(); // an array of objects
-			$this->_fields = array(); // an array of strings
-			// and populate the collection
-			$this->load();
-
+		if ($origin == null)
+			$this->_origin = get_class($this);
+		else
+			$this->_origin = $origin;
+		// remember the other constructor fields
+		$this->_keyfield = $keyfield;
+		$this->_entity = $entity;
+		// start with an empty collection
+		$this->_data = array(); // an array of objects
+		$this->fields = array(); // an array of strings
+		// and populate the collection
+		$this->load();
 	}
-
-			/**
+	/**
 	 * Load the collection state appropriately, depending on persistence choice.
 	 * OVER-RIDE THIS METHOD in persistence choice implementations
 	 */
 	protected function load()
 	{
-
-		//var_dump(realpath($this->_origin));
+				//var_dump(realpath($this->_origin));
 		if (file_exists(realpath($this->_origin))) {
-
 		    $this->xml = simplexml_load_file(realpath($this->_origin));
 		    if ($this->xml === false) {
 			      // error so redirect or handle error
 			      header('location: /404.php');
 			      exit;
 			}
-
 		    $xmlarray =$this->xml;
-
 		    //if it is empty; 
 		    if(empty($xmlarray)) {
 		    	return;
 		    }
-
 		    //get all xmlonjects into $xmlcontent
 		    $rootkey = key($xmlarray);
 		    $xmlcontent = (object)$xmlarray->$rootkey;
-
 		    $keyfieldh = array();
 		    $first = true;
-
 		    //if it is empty; 
 		    if(empty($xmlcontent)) {
 		    	return;
 		    }
-
 		    $dataindex = 1;
 		    $first = true;
 		    foreach ($xmlcontent as $oj) {
@@ -76,43 +75,33 @@ class XML_Model extends Memory_Model {
 			    	$this->_fields = $keyfieldh;
 			    }
 		    	$first = false; 
-
 		    	//var_dump($oj->children());
 		    	$one = new stdClass();
-
 		    	//get objects one by one
 		    	foreach ($oj as $key => $value) {
 		    		$one->$key = (string)$value;
 		    	}
 		    	$this->_data[$dataindex++] =$one; 
 		    }	
-
-
 		 	//var_dump($this->_data);
 		} else {
 		    exit('Failed to open the xml file.');
 		}
-
 		// --------------------
 		// rebuild the keys table
 		$this->reindex();
 	}
-
 	/**
 	 * Store the collection state appropriately, depending on persistence choice.
 	 * OVER-RIDE THIS METHOD in persistence choice implementations
 	 */
 	protected function store()
 	{
-		    // var_dump(get_class_methods($this->xml));
-		    // var_dump($this->xml->getName);
-		    
-        	
+		        	
 		// rebuild the keys table
 		$this->reindex();
  		if (($handle = fopen($this->_origin, "w")) !== FALSE)
         {
-
             $xmlDoc = new DOMDocument( "1.0");
             $xmlDoc->preserveWhiteSpace = false;
             $xmlDoc->formatOutput = true;
@@ -130,6 +119,6 @@ class XML_Model extends Memory_Model {
             $xmlDoc->saveXML($xmlDoc);
             $xmlDoc->save($this->_origin);
         }
+	}
 
-}
 }
--- application/models/Tasks.php
+++ application/models/Tasks.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * Modified to use REST client to get port data from our server.
+ */
+define('REST_SERVER', 'http://backend.local');  // the REST server host
+define('REST_PORT', $_SERVER['SERVER_PORT']);   // the port you are running the server on
+
 
 /*Data model class for tasks.csv table.*/
 class Tasks extends XML_Model {
@@ -8,6 +14,65 @@ class Tasks extends XML_Model {
 	{
 		parent::__construct(APPPATH . '../data/tasks.xml', 'id');
 	}
+
+	protected function load()
+	{
+
+		// load the $this->_data array
+		// load our data from the REST backend
+        $this->rest->initialize(array('server' => REST_SERVER));
+        $this->rest->option(CURLOPT_PORT, REST_PORT);
+        $this->_data =  $this->rest->get('job');
+
+		// rebuild the field names from the first object
+		$one = array_values((array) $this->_data);
+		$this->_fields = array_keys((array)$one[0]);
+
+		// rebuild the keys table
+		$this->reindex();
+	}
+
+	protected function store()
+	{
+	}
+
+		// Retrieve an existing DB record as an object
+	function get($key, $key2 = null)
+	{
+        $this->rest->initialize(array('server' => REST_SERVER));
+        $this->rest->option(CURLOPT_PORT, REST_PORT);
+        return $this->rest->get('job/' . $key);
+	}
+
+	// Delete a record from the DB
+	function delete($key, $key2 = null)
+	{
+        $this->rest->initialize(array('server' => REST_SERVER));
+        $this->rest->option(CURLOPT_PORT, REST_PORT);
+        $this->rest->delete('job/' . $key);
+        $this->load(); // because the "database" might have changed
+	}
+
+	// Update a record in the DB
+	function update($record)
+	{
+        $this->rest->initialize(array('server' => REST_SERVER));
+        $this->rest->option(CURLOPT_PORT, REST_PORT);
+        $key = $record->{$this->_keyfield};
+        $retrieved = $this->rest->put('job/' . $key, $record);
+        $this->load(); // because the "database" might have changed
+	}
+
+	// Add a record to the DB
+	function add($record)
+	{
+        $this->rest->initialize(array('server' => REST_SERVER));
+        $this->rest->option(CURLOPT_PORT, REST_PORT);
+        $key = $record->{$this->_keyfield};
+        $retrieved = $this->rest->post('job/' . $key, $record);
+        $this->load(); // because the "database" might have changed
+	}
+
 
 	function makeCategorizedPanel($tasks)
 	{
